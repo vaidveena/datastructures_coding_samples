38abe3b57bb135a8146ba83d8a6fa038b020258c
Veena Vaidyanathan <vvaidyanathan@groupon.com>
2016-04-28 11:59:11 -0700

    adding an example for twoSum

---
+++ java_datastructures/TwoSum.java
@@ -0,0 +1,39 @@
+package datastructure_coding;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+
+/**
+ * @author vvaidyanathan
+ *
+ */
+public class TwoSum {
+
+    public ArrayList<Integer> twoSum(final List<Integer> a, int b) {
+        
+        ArrayList<Integer> result = new ArrayList<Integer>();
+                
+        if (a.size() < 2)
+            return result;
+
+        HashMap<Integer, Integer> h = new HashMap<Integer, Integer>();
+        for (int i=0; i < a.size(); i++)
+        {
+            int diff = b - a.get(i);
+            if (h.containsKey(diff))
+            {
+                int index = h.get(a.get(diff));
+                result.add(index+1);
+                result.add(i+1);
+                break;
+            }
+            else
+            {
+                h.put(a.get(i), i);
+            }
+        }
+        return result;
+    }
+
+}
